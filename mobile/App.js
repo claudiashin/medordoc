77fbbe9446246a52a61fbe743164b605148dceb8
Iori Takeshita <takeshitaiori@gmail.com>
2021-10-27 23:54:13 -0700

    calendar

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Appointment from './comps/Calendar'
 
+
+// export {default} from './storybook'; 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello.js to start working on your app!</Text>
+      <Appointment/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/comps/Calendar/index.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+
+
+const Appointment =({})=>{
+return(
+<Calendar
+onDayPress={(day) => {console.log('selected day', day)}}/>
+)
+}
+export default Appointment;
