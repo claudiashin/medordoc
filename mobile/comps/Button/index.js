5b933eca26e9b5d9a48bf3a4fe1d53574f5eff38
leighai <vnishibata@my.bcit.ca>
2021-10-27 23:52:30 -0700

    first commit - added header comp and started on button comp for mobile

---
+++ mobile/comps/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
