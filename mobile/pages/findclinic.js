8b4e2a7669e2e4560ad49834a6b79ace19413e1d
willusmac <wmcdonald26@my.bcit.ca>
2021-11-04 22:13:28 -0700

    clinic profile page started/emailapi attached to button

--- mobile/pages/findclinic.js
+++ mobile/pages/findclinic.js
@@ -9,7 +9,8 @@ import ClinDocButton from '../comps/ClinDocButton'
 
   const Cont = styled.View`
   width:100%;
-  height:400px;
+  height:100%;
+  background-color: #E9D7CB;
   display:flex;
   align-content:center;
   justify-content:center;
@@ -21,7 +22,7 @@ return(
    <Cont>
     <SearchBar/>   
     <Map/>
-    {/* <Filiter/> */}
+    <Filiter/>
    
     </Cont> 
 )
