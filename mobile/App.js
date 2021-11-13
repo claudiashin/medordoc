67c2b6c4326b5f0ee98c8e29a976b86185f746bb
leighai <vnishibata@my.bcit.ca>
2021-11-12 18:54:59 -0800

    apps page changes

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useState,useEffect} from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import styled from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,7 +15,6 @@ import BookingForm from './comps/BookingForm';
 import home from './pages/home';
 import findclinic from './pages/findclinic';
 import clinicprofile from './pages/clinicprofile';
-// import login from './pages/login';
 import signup from './pages/signup';
 import accountconfirm from './pages/accountconfirm';
 import booking from './pages/booking';
@@ -24,20 +23,11 @@ import scan from './pages/scan';
 import finddoc from './pages/finddoc';
 import history from './pages/history';
 import docprofile from './pages/docprofile';
+import login from './pages/login';
 
-// import login from './pages/login';
-import accountconfirm from './pages/accountconfirm';
-import signup from './pages/signup';
 import patientprofile from './pages/patientprofile';
-import history from './pages/history';
-import scan from './pages/scan';
-
-import finddoc from './pages/finddoc';
-import docprofile from './pages/docprofile';
 import confirmreq from './pages/confirmreq';
-
 import QR from './pages/QR';
-
 
 
 const Stack = createNativeStackNavigator();
@@ -45,30 +35,27 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-  
-      <NavigationContainer>
-       <Stack.Navigator initialRouteName="home">
-         <Stack.Screen name="home" component={home} />
-         <Stack.Screen name="QR" component={QR} /> 
-         <Stack.Screen name="findclinic" options={{headerShown: false}}   component={findclinic} /> 
-         <Stack.Screen name="finddoc" component={finddoc} />
-         <Stack.Screen name="clinicprofile" component={clinicprofile} />
-         <Stack.Screen name="patientprofile" component={patientprofile} />
-         <Stack.Screen name="signup" component={signup} />
-         <Stack.Screen name="booking" component={booking} />
-         <Stack.Screen name="qrconfirm" component={qrconfirm} />
-         <Stack.Screen name="QR" component={scan} />
-        <Stack.Screen name="accountconfirm" component={accountconfirm}/> 
-        <Stack.Screen name="login" component={login}/> 
 
-         <Stack.Screen name="scan" component={scan} /> 
-         <Stack.Screen name="login" component={login} /> 
-         <Stack.Screen name="history" component={history} /> 
-         <Stack.Screen name="confirmreq" component={confirmreq} />
-       </Stack.Navigator>
-     </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name="home" component={home} />
+        <Stack.Screen name="QR" component={QR} />
+        <Stack.Screen name="findclinic" options={{ headerShown: false }} component={findclinic} />
+        <Stack.Screen name="finddoc" component={finddoc} />
+        <Stack.Screen name="clinicprofile" component={clinicprofile} />
+        <Stack.Screen name="patientprofile" component={patientprofile} />
+        <Stack.Screen name="signup" component={signup} />
+        <Stack.Screen name="booking" component={booking} />
+        <Stack.Screen name="qrconfirm" component={qrconfirm} />
+        <Stack.Screen name="accountconfirm" component={accountconfirm} />
+        <Stack.Screen name="login" component={login} />
+        <Stack.Screen name="scan" component={scan} />
+        <Stack.Screen name="history" component={history} />
+        <Stack.Screen name="confirmreq" component={confirmreq} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-      );
+  );
 }
 
 const styles = StyleSheet.create({
